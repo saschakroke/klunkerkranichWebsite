b4e1ebca9ee782215f4d134e99569efde8837026
saschakroke <s.krokee@gmail.com>
2023-01-24 16:00:38 +0100

    test

--- app.py
+++ app.py
@@ -1,29 +1,46 @@
 from flask import Flask, render_template, request
-from backend.klassen.User import NewUser
+from flask_login import login_required
 from backend.controller.login import checkLogin
+from backend.klassen.User import NewUser
+
+
 import os
-#pfad noch anpassen bevors aufn server geht
+
+
+
+ #pfad noch anpassen bevors aufn server geht
 template_dir = os.getcwd()
-template_dir = os.path.join(template_dir, 'frontend')
-template_dir = os.path.join(template_dir, 'templates')
+template_dir = os.path.join(template_dir, 'KlunkerkranichWebsite/frontend/templates')
 app = Flask(__name__, template_folder=template_dir)
+
+
 @app.route("/")
 def index():
     return render_template('/index.html')
-@app.route("/login")
-def login():
-    return render_template('/login.html')
 
-@app.route("/welcome", methods=['POST'])
-def welcome():
-    email = request.form.get("email")
-    password = request.form.get("password")
-    user = NewUser(email, password)
-    response = checkLogin(user)
-    if response:
-        return render_template('/success.html', email=email)
+
+@app.route("/login", methods=['GET',' POST'])
+def login():
+    email =  request.form.get('email')
+    password =  request.form.get('password')
+
+    loginUser = NewUser(email, password)
+    
+    login_response = checkLogin(loginUser,app)
+
+    if login_response:
+        return render_template('/index.html')
     else:
-        return render_template('/error.html', email=email)
+        return render_template('/login.html')
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
--- backend/controller/login.py
+++ backend/controller/login.py
@@ -1,12 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_user, LoginManager, login_required, logout_user, current_user
+from flask_bcrypt import Bcrypt
 from backend.klassen.User import NewUser
 
 
-def checkLogin(user):
-    databaseObject = NewUser("ben@kro.de", "123")
 
-    if user.email == databaseObject.email and user.password == databaseObject.password:
-        return True
+
+
+def checkLogin(loginUser, app):
+   
+    testUserDb = NewUser("ben@kro.de","1123")
+    
+    
+    if loginUser.email == testUserDb.email and loginUser.password == testUserDb.password:
+       
+            login_manager = LoginManager()
+            login_manager(app)
+            login_user(loginUser)
+            print("is drin")
+            return True
+          
     else:
+        print("is nicht drin")
         return False
 
 
